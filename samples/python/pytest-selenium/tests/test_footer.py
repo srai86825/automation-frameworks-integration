d266f1312c541f6308d696cde46b131a73865eb6
Saurabh Kumar Rai <srai86825@gmail.com>
2024-07-09 10:07:42 -0700

    Update test_footer.py by subid

--- samples/python/pytest-selenium/tests/test_footer.py
+++ samples/python/pytest-selenium/tests/test_footer.py
@@ -5,5 +5,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 class TestsFooter:
 
     @staticmethod
-    def test_social_links(browser_driver: WebDriver):
+    @pytest.mark.subid("sub-101")
+    def test_social_links(browser_driver: WebDriver, record_property):
+        record_property("subid", "subid-101")
         assert browser_driver.find_element(By.XPATH, "//*[@href='https://www.linkedin.com/company/testrail/']").is_displayed()
