6ec4c26f38c52bf88dc6ae3c40fd84c09af18a63
Saurabh Kumar Rai <srai86825@gmail.com>
2024-07-09 10:07:03 -0700

    Update test_homepage.py
    added subid

--- samples/python/pytest-selenium/tests/test_homepage.py
+++ samples/python/pytest-selenium/tests/test_homepage.py
@@ -5,10 +5,12 @@ from selenium.webdriver.remote.webdriver import WebDriver
 class TestsHomepage:
 
     @staticmethod
+    @pytest.mark.subid("sub-102")
     def test_header_links(browser_driver: WebDriver, record_property):
         """
         Checks headers links
         """
+        record_property("subid", "subid-102")
         record_property("testrail_result_comment", "1. Check Platform link")
         assert browser_driver.find_element(By.XPATH, "//*[@href='/platform/']").is_displayed()
 
@@ -19,12 +21,10 @@ class TestsHomepage:
         assert browser_driver.find_element(By.XPATH, "//*[@href='/pricing/']").is_displayed()
 
     @staticmethod
+    @pytest.mark.subid("subid-103")
     def test_action_links(browser_driver: WebDriver, record_property):
         """
         Checks actions links for trial and demo
         """
-        record_property("testrail_result_comment", "1. Check Demo link")
-        assert browser_driver.find_element(By.XPATH, "(//*[contains(@href,'webinar/register')])[1]").is_displayed()
-
-        record_property("testrail_result_comment", "2. Check Trial link")
-        assert browser_driver.find_element(By.XPATH, "//*[@href='/get_trial_invalid_link/']").is_displayed()
+        record_property("subid", "subid-103")
+        record_property("tes
