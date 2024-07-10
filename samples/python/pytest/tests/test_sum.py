26dbc6bffe7d006c096e9c3cd2578c3f0e258795
Saurabh Kumar Rai <srai86825@gmail.com>
2024-07-10 02:21:03 -0700

    Update test_sum.py failed one

--- samples/python/pytest/tests/test_sum.py
+++ samples/python/pytest/tests/test_sum.py
@@ -8,8 +8,8 @@ def test_sum_two_numbers(record_property):
 def test_sum_two_decimals(record_property):
     # Adding a subid for the test case
     record_property("subid", "sub-103")
-    # assert 0.8 + 0.3 == 1.2
-    assert 0.8 + 0.3 == 1.1
+    assert 0.8 + 0.3 == 1.2
+    # assert 0.8 + 0.3 == 1.1
 
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
 def test_sum_multiple_numbers(record_property, test_input, expected):
