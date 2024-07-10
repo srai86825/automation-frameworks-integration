dd7e3c79cc1977473916f94a2183a63322cc4399
Saurabh Kumar Rai <srai86825@gmail.com>
2024-07-10 04:02:21 -0700

    Update test_subtraction.py subid changed

--- samples/python/pytest/tests/test_subtraction.py
+++ samples/python/pytest/tests/test_subtraction.py
@@ -1,4 +1,4 @@
 def test_subtract_two_numbers(record_property):
     # Adding a subid for the test case
-    record_property("subid", "sub-101")
+    record_property("subid", "JE00")
     assert 1 - 1 == 0
