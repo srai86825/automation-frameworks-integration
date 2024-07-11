e4249314eb0199feb222ee2cffa43ebdd794c556
Saurabh Kumar Rai <srai86825@gmail.com>
2024-07-11 08:40:56 -0700

    Update test_sum.py

--- samples/python/pytest/tests/test_sum.py
+++ samples/python/pytest/tests/test_sum.py
@@ -2,17 +2,17 @@ import pytest
 
 def test_sum_two_numbers(record_property):
     # Adding a subid for the test case
-    record_property("subid", "JE11")
+    record_property("subid", "JE21")
     assert 1 + 1 == 2
 
 def test_sum_two_decimals(record_property):
     # Adding a subid for the test case
-    record_property("subid", "JE13")
+    record_property("subid", "JE20")
     assert 0.8 + 0.3 == 1.2
     # assert 0.8 + 0.3 == 1.1
 
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
 def test_sum_multiple_numbers(record_property, test_input, expected):
     # Adding a subid for the test case
-    record_property("subid", "JE12")
+    record_property("subid", "JE22")
     assert eval(test_input) == expected
